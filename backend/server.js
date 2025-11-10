7d2c9940c8f3a2a56abee4e0feeb617836e03860
Neerajprajapat15 <156994644+Neerajprajapat15@users.noreply.github.com>
2025-11-11 01:44:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/users", userRoutes);
 
 // for production
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
     app.get("*all", (req, res) =>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
     })
 }
 
